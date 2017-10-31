58de641d8a0af86078b70a0422be2876396fdd33
Alex Sellers <czar@1x-nat-vl930-172-30-74-224.wireless.umass.edu>
2017-10-31 13:57:19 -0400

    fixed more unresolved merge conflicts left in code

--- part2/SQL_Injection/SpoTV/admin.py
+++ part2/SQL_Injection/SpoTV/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Song, YoutubePlaylist
+from .models import Song, YoutubePlaylist, Playlist, User
 # Register your models here.
-admin.site.register(Song)
 admin.site.register(YoutubePlaylist)
-=======
-from .models import *
 # Register your models here.
 class SongAdmin(admin.ModelAdmin):
     list_display = ('title', 'artist', 'album', 'genre', 'date', 'hasVideo')
@@ -16,4 +12,3 @@ admin.site.register(Playlist)
 class UserAdmin(admin.ModelAdmin):
     list_display =  ('fname', 'lname', 'usrname', )
 admin.site.register(User, UserAdmin)
->>>>>>> 0cb049b6af3b3024a2a38f59cc4f0d6334c2ef17
