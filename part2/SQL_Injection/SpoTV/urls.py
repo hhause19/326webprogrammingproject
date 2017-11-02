590699c49e53e5b8844d29979a9a4ef27e5478ec
Ruifeng Zhang <ruifeng.zhang1993@gmail.com>
2017-11-02 01:07:40 -0400

    add change to preference

--- part2/SQL_Injection/SpoTV/urls.py
+++ part2/SQL_Injection/SpoTV/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
      url(r'^myplaylists/$', views.myplaylists, name='playlists'),
      url(r'^myplaylists/(?P<pk>\d+)$', views.myplaylistdetail, name='playlistdetail'),
      url(r'^accinfo/$', views.accinfo, name='accinfo'),
+     url(r'^preference/$', views.preference, name='prefer'),
 ]
