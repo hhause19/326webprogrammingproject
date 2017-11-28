3e463d2642b0c7171ce9e398acecdb53152adcfa
wsattanuparp <willsattanuparp@gmail.com>
2017-11-28 00:05:03 -0500

    fixed rendering issue

--- project2/SQL_Injection/SpoTV/urls.py
+++ project2/SQL_Injection/SpoTV/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
      url(r'^preference/$', views.preference, name='prefer'),
      url(r'^search/$', views.playlist_filter, name = 'search'),
      url(r'^search/(?P<pk>\d+)$', views.myplaylistdetail, name = 'playlistdetail'),
-     url(r'^accounts/', include('django.contrib.auth.urls')),
      url(r'^password/$', views.change_password, name='change_password'),
+     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
--- project2/SQL_Injection/SpoTV/views.py
+++ project2/SQL_Injection/SpoTV/views.py
@@ -155,11 +155,13 @@ def change_password(request):
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
-            update_session_auth_hash(request, user)
+            update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
             return redirect('change_password')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
         form = PasswordChangeForm(request.user)
-    return render(request, 'accinfo.html', {'form': form})
+    return render(request, 'accinfo.html', {
+        'form': form
+    })
